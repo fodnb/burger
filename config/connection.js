ed8a25fa5132b35aebfafa51af9d3e5b2fd77740
daniel mcghee <daniel mcghee>
2017-04-29 17:18:39 -0400

    heroku still

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,55 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: " otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: " so6s2ykuazm4p8of",
-    password: " k7jx03r8r0mynxxu",
-    database: "burgers_db",
-    port: 3306
-});
+// // var connection = mysql.createConnection({
+// //     host: " otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+// //     user: " so6s2ykuazm4p8of",
+// //     password: " k7jx03r8r0mynxxu",
+// //     database: "burgers_db",
+// //     port: 3306
+// // });
 
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
-// connection.connect();
+// // var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// // connection.connect();
+
+// if(process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else{
+//     connection = mysql.createConnection({
+//     host: "otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     user: "so6s2ykuazm4p8of",
+//     password: "k7jx03r8r0mynxxu",
+//     database: ""
+//     });
+// };
+
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
+
+// module.exports = connection;
+
+var connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        root: 3000,
+        host: 'localhost',
+        user: 'root',
+        password: 'Buddy#13',
+        database: 'burgers_db',
+    });
+};
+
 connection.connect(function(err) {
     if (err) {
-        console.error("error connecting: " + err.stack);
+        console.error('error connecting: ' + err.stack);
         return;
     }
-    console.log("connected as id " + connection.threadId);
+    console.log('connected as id ' + connection.threadId);
 });
-// if(process.env.JAWSDB_URL){
-
-// }else {
-
-// }
-
-
-module.exports = connection;
-
