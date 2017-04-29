10b03623b39cb5997604af39465352c68229f77f
daniel mcghee <daniel mcghee>
2017-04-29 16:20:05 -0400

    working on getting heroku working

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,29 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Buddy#13",
-    database: "burgers_db"
+    host: " otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: " so6s2ykuazm4p8of",
+    password: " k7jx03r8r0mynxxu",
+    database: "burgers_db",
+    port: 3306
 });
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// connection.connect();
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
+if(process.env.JAWSDB_URL){
+
+}else {
+    
+}
+
+mysql://so6s2ykuazm4p8of:k7jx03r8r0mynxxu@otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ejvimz4osj2gxy3x
 
 module.exports = connection;
 
--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var PORT = process.env.PORT || 3000;
-
+// var PORT = process.env.PORT || 3000;
+var PORT = process.env.JAWSDB_URL || 3000;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
+// app.use(express.static(process.cwd() + "/public"));
+app.use("/cssfiles", express.static(__dirname + "/public/assets"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
