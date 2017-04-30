f779eaf9c690ae8fd0b79d2c921d8873ea3dfd63
daniel mcghee <daniel mcghee>
2017-04-30 07:34:56 -0400

    still heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-// var PORT = process.env.PORT || 3000;
-var PORT = process.env.JAWSDB_URL || 3000;
+var PORT = process.env.PORT || 3000;
+// var PORT = process.env.JAWSDB_URL || 3000;
 
 var app = express();
 
