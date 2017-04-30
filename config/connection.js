de080812975585e3f21b74287cf797eee9a88417
daniel mcghee <daniel mcghee>
2017-04-30 13:41:01 -0400

    added var port

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 var mysql = require("mysql");
 
-
+var PORT = process.env.PORT || 3306;
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Buddy#13',
     database: 'burgers_db',
-    port:  process.env.PORT || 3306
+    port:  PORT
 
 });
 
