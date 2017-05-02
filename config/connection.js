ea2707ce60958058514d85ce6a822170b6fa2e5d
daniel mcghee <daniel mcghee>
2017-05-02 18:12:32 -0400

    redeploy

--- config/connection.js
+++ config/connection.js
@@ -20,11 +20,5 @@ if (process.env.JAWSDB_URL) {
 //     port:  PORT
 // });
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
+connection.connect();
 module.exports = connection;
